4bc995fe4603dcfe9d96ce432f099460f5c4427b
Jonathan Fernandes <jhfernan@mail.usf.edu>
2018-02-15 13:41:31 -0500

    Improvements to the parralel file

--- Mike's Playground/parallel_plate.py
+++ Mike's Playground/parallel_plate.py
@@ -6,7 +6,7 @@ import math
 #store values
 
 #Permittivity constant
-perm = 8.854187817e-12
+permittivity = 8.854187817e-12
 
 #ADDITIONAL FUNCTIONALITY
 #Ask user if electric field value is available
@@ -15,13 +15,22 @@ perm = 8.854187817e-12
 
 #Electric Field of a parallel plate capacitor formula (E = eta/Epsilon Naught)
 
+def get_float_value(value_type):
+	value = None
+	while not value:
+		try:
+			value = float(input('Enter the {} value.\n>>> '.format(value_type)))
+		except ValueError:
+			print('Please enter a valid decimal number.')
+	return value
+
 def pp_electric_field():
-    #In order to calculate eta we need the charge value and the area.
-    charge = float(input('Enter the charge value.\n>>> '))
-    area = float(input('Enter your area value.\n>>> '))
-    charge_density = charge/area
-    E_field = eta/perm
-    return E_field
+	#In order to calculate eta we need the charge value and the area.
+	charge = get_float_value('charge')
+	area = get_float_value('area')
+	charge_density = charge/area
+	electric_field = charge_density/permittivity
+	return electric_field
 
 result = pp_electric_field()
 print(result)
@@ -29,9 +38,9 @@ print(result)
 #delta V = Ed (being the voltage difference between two plates. This equals the electric field times the distance between them.)
 
 def electric_potential():
-    dist = float(input('What is the distance or separation between the capacitor plates?\n>>> '))
-    change_in_V = result * dist
-    return cng_volt
+	distance = float(input('What is the distance or separation between the capacitor plates?\n>>> '))
+	change_in_voltage = result * distance
+	return change_in_voltage
 
 voltage = electric_potential()
 print(voltage)
